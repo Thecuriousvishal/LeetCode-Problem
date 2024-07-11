7c5b20926ba490b2cd519170e70baf3c736a29f8
Vishal Kumar <154529583+Thecuriousvishal@users.noreply.github.com>
2024-07-12 01:20:26 +0530

    Time: 2 ms (53.59%), Space: 7.5 MB (38.21%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        int rounds=time/(n-1);
-        int ans=0;
-        if(rounds%2==0){
-            ans=1+time%(n-1);
-        }else{
-            ans=(n-time%(n-1));
+        // int rounds=time/(n-1);
+        // int ans=0;
+        // if(rounds%2==0){
+        //     ans=1+time%(n-1);
+        // }else{
+        //     ans=(n-time%(n-1));
+        // }
+        // return ans;
+    
+    
+    
+    
+     int current_person = 1;  // Start from person 1
+     int direction = 1;       // Direction: 1 for forward, -1 for backward
+
+     for (int i = 0; i < time; ++i) {
+        if (current_person == 1) {
+            direction = 1;  // Start moving forward
+        } else if (current_person == n) {
+            direction = -1; // Start moving backward
         }
-        return ans;
+        
+        current_person += direction;
+    }
+
+    return current_person;
+        
     }
 };

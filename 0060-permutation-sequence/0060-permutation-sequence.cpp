2a94bb4b3a4373a4c861d35c5ad49beba02f9606
Vishal Kumar <154529583+Thecuriousvishal@users.noreply.github.com>
2024-10-14 22:22:29 +0530

    Time: 3 ms (50.34%), Space: 8.4 MB (35.33%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+
+        vector<int> numbers;
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            numbers.push_back(i);
+            factorial *= i;
+        }
+        k--;
+
+        // Initialize an empty result string
+        string result = "";
+
+        for (int i = 0; i < n; i++) {
+            factorial /= (n - i);
+            int index = k / factorial;
+            result += to_string(numbers[index]);
+            numbers.erase(numbers.begin() + index);  
+            k %= factorial;  
+        }
+
+        return result;
+    }
+};
